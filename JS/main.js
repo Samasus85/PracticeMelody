ec12170b55d386dcf154388bfa9cf1f8540b4703
Samasus85 <89699068+Samasus85@users.noreply.github.com>
2021-12-13 13:01:50 +0300

    4day

--- JS/main.js
+++ JS/main.js
@@ -1,14 +1,25 @@
 $(document).ready(function () {
-    let currentFloor = 2
-    let floorPath = $(".home-image path")
-    let counterUp = $(".counter-up")
-    let counterDown = $(".counter-down")
+    let currentFloor = 2;
+    let floorPath = $(".home-image path");
+    let counterUp = $(".counter-up");
+    let counterDown = $(".counter-down");
+    let modal = $(".modal");
+    let modalCloseButton = $(".modal-close-button");
+    let viewFlatsButton = $(".view-flats")
+
+
     floorPath.on("mouseover", function () {
         floorPath.removeClass("current-floor")
 
         currentFloor = $(this).attr("data-floor")
         $(".counter").text(currentFloor);
     })
+
+    floorPath.on("click", toggleModal);
+
+    modalCloseButton.on("click", toggleModal);
+    viewFlatsButton.on("click", toggleModal)
+
     counterUp.on("click", function () {
         if (currentFloor < 18) {
             currentFloor++;
@@ -29,4 +40,8 @@ $(document).ready(function () {
         }
 
     })
+    function toggleModal() {
+        modal.toggleClass("is-open")
+    }
 });
+
